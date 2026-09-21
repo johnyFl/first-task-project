77cba59170751e712ada5e0b766073aa16be5ae2
john <wyngreece7@yahoo.gr>
2026-09-21 14:58:37 +0300

    decorators

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ users = [
     {"name": "Giorgos", "active": True},
     {"name": "Nikos", "active": True},
 ]
+def logger(func):
+    def wrapper(users):
+        print("start")
+        yield from func(users)
+        print("end")
+    return wrapper
 
+@logger
 def active_users(users):
     for user in users:
         if user["active"]:
             yield user
+
 for user in active_users(users):
     print(user)
