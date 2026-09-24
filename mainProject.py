492ae2dbdbf6159912ad809e371b89b5448cead7
john <wyngreece7@yahoo.gr>
2026-09-24 20:07:06 +0300

    userService2

--- mainProject.py
+++ mainProject.py
@@ -8,6 +8,7 @@ class User():
         self.active=active    
     def __str__(self):
         return(f"{self.id} {self.name} {self.email} {self.active}" )
+    
 class Admin(User):
     def __init__(self, id, name, email, active):
          super().__init__(id, name, email, active)
@@ -16,6 +17,7 @@ class Admin(User):
             return(f"{self.id} {self.name} {self.email} {self.active} {self.role}" )  
     def permissions(self):
          return ["create_user", "delete_user", "view_users"]
+    
 class Logger():
      pass
 class UserService():
@@ -25,17 +27,34 @@ class UserService():
         for user in users:
             if user.active:
              yield user
+def logger(func):
+    def wrapper(*args,**kargs):
+        print("START")
+        result=func(*args,**kargs)
+        print("END")
+        return result
+    return wrapper
 
-
-
-user1 = User(1, "Nikos", "nikos@test.com", False)
-user2 = User(1, "Nikos", "nikos@test.com", True)
 users=[]
-users.append(user1)
-users.append(user2)
+
+@logger
+def createUsers():
+    print("users creation")
+    user1 = User(1, "Nikos", "nikos@test.com", True)
+    user2 = User(2, "giannis", "giannis@test.com", False)
+    admin=Admin(1, "kostas", "kostas@test.com", True)
+    users.append(admin)
+    users.append(user1)
+    users.append(user2)
+
+@logger
+def getUsers():
+    print("users get")
+    return users
+
+createUsers()
+users=getUsers()
+
 service=UserService()
 for user in service.activeUsers(users):
     print(user)
-admin=Admin(1, "giannis", "giannis@test.com", True)
-print (admin)
-print (admin.permissions())
